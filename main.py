235e8d9197482a8636f4c2fe7bb9b25a0077ec41
Jack Case <casejwork@gmail.com>
2025-02-06 12:30:39 +1100

    Player class finished

--- main.py
+++ main.py
@@ -3,22 +3,48 @@
 # throughout this file
 import pygame
 
+#holds the game constants
 from constants import *
+
+#player class
+from player import Player
+
+
 
 
 
 def main():
-    #print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    # game clock for fps
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #class variable
+    Player.containers = (updatable, drawable)
+
+    # inits screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #declares player
+    player_spaceship = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+
+    #infite while loop to create the board and player
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        updatable.update(dt)    
+        screen.fill("black")        
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
